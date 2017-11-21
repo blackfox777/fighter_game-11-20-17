1f2b047c5e5567dcacd169036630acde35a366c7
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-20 20:51:11 -0500

    added eresed html

---
+++ js/script.js
@@ -0,0 +1,40 @@
+class Fighter {
+  constructor(name, weapon, armor, moves){
+      this.weapon = ('guantlet', 'swords', 'grimoire';
+      this.weapon =  helmet;
+      this.slogan = slogan;
+      this.specialMove = ()
+      this.armor= true;
+
+  }
+
+}
+
+var slogana ="Time to brawl";
+var sloganb ="Charge up";
+var sloganc ="All I need is one slash";
+
+function battleRoyal(fightera,fighterb,fighterc) {
+  console.log(fightera.slogan);
+  console.log(fighterb.slogan);
+  console.log(fighterc.slogan);
+
+  if (fightera.weapon. === fighterb.weapon[0]) {
+    console.log()
+  }
+    else if(fighter1a.weapon[1] && fighterb.weapon[1] {
+      console.log()
+  }
+  else if(fighterc.weapon[1] && fighterb.weapon[2] {
+    console.log(fighterc.name + "wins");
+  }
+}
+
+
+var Ryu = new Fighter('Ryu Kruger', Gauntlet, armor, );
+var Jolt = new Fighter('Jolt Shockman', grimoire, armor);
+var Slash = new Fighter('slash Cuttman', sword, armor);
+
+
+  }
+}
